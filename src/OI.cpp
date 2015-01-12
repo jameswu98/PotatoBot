fe4c55daa3ffad03a0d31c927403816d6eaf482b
James Wu <james.wu.wu1@gmail.com>
2015-01-12 16:22:35 +1100

    Finished doing the IO, only have to finish the command cpp file now...

--- src/OI.cpp
+++ src/OI.cpp
@@ -1,7 +1,24 @@
 #include "OI.h"
 
 OI::OI() {
-    // Process operator interface input here.
+    // Make a new Joystick on Port 1.
     JoyDrv = new Joystick(1);
+    // Process operator interface input here.
+}
 
+
+double OI::getJoyDrvX() {
+    return JoyDrv->GetX();
+}
+
+double OI::getJoyDrvY() {
+    return JoyDrv->GetY();
+}
+
+double OI::getJoyDrvR() {
+    return JoyDrv->GetTwist();
+}
+
+double OI::getJoyDrvThrottle() {
+    return JoyDrv->GetThrottle();
 }
